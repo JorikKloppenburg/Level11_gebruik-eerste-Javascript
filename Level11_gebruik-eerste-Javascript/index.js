0fed6783c9431b66eaa96fe8392ef02af5f581a5
Jorik Kloppenburg <78646379+JorikKloppenburg@users.noreply.github.com>
2021-05-28 12:21:08 +0200

    Add files via upload

---
+++ Level11_gebruik-eerste-Javascript/index.js
@@ -0,0 +1,41 @@
+//Dit is mijn eerste Javascriptcode
+let naam = "Jorik Kloppenburg";
+console.log(naam);
+
+
+// Twee numbers bij elkaar optellen is wel mogelijk
+let numbersOptellen = 4 + 4;
+console.log(numbersOptellen);
+// Resultaat = 8
+
+// Twee Strings bij elkaar optellen niet mogelijk, enkel de tekst wordt weergegeven
+let stringsOptellen = "4" + "4";
+console.log(stringsOptellen);
+// Resultaat is 44
+
+// optellen
+let addition = 100 + 4;
+console.log(addition);
+
+// aftrekken
+let subtraction = 100 - 4;
+console.log(subtraction);
+
+// vermenigvuldigen
+let multiplication = 100 * 4;
+console.log(multiplication);
+
+// delen
+let division = 100 / 4;
+console.log(division);
+
+// percentage
+let modulus = 4 % 100;
+console.log(modulus);
+
+// leeftijd van number naar string
+let myAge = "26";
+console.log(myAge);
+console.log(typeof myAge);
+
+
